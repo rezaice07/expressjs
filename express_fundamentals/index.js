0191b6c53b1440bbcaaed8a4d52dd57e4a3c01c4
Rejwanul Reja <rezaice07@gmail.com>
2022-02-07 09:21:30 +0600

    added js for req

--- express_fundamentals/index.js
+++ express_fundamentals/index.js
@@ -1,29 +1,39 @@
 const express=require('express');
 const app=express();
-
-//installation of ejs
-// npm i ejs 
-
-app.set('view engine','ejs');
-//set method for ejs view engine 
-
-app.route('/event')
-.get((req,res)=>{    
-  res.render('event/list');
-  //here render instead of send
-})
-.post((req,res)=>{    
-  res.send('This is event page with Post request');
-})
-.put((req,res)=>{    
-  res.send('This is event page with put request');
-})
-.delete((req,res)=>{    
-  res.send('This is event page with Delete request');
+ 
+//child route
+const clientRout=express.Router();
+clientRout.get('/dashboard',(req,res)=>{
+  console.log(req.baseUrl); //  /client
+  console.log(req.originalUrl); //  /client/dashboard
+  console.log(req.url);   //  /dashboard
+  console.log(req.path); // /dashboard
+  console.log(req.hostname);
+  console.log(req.method);
+  res.send('This is client dashbaord');
 });
+//client reoute
+app.use('/client',clientRout);
 
+
+
+//root/main route
+app.get('/user/:id',(req,res)=>{  
+  //request url: localhost:3001/user/5454?username=rejawebs
+  console.log(req.baseUrl); // blank
+  console.log(req.originalUrl);  //here original url: /user/5454?username=rejawebs
+  console.log(req.url);   //here url: /user/5454?username=rejawebs
+  console.log(req.path); // only removed querystring portion: /user/5454 
+  console.log(req.hostname); // always same for both root and child route
+  console.log(req.protocol); // http
+  console.log(req.params); // { id: '5454' }
+  console.log(req.query); // { username: 'rejawebs' }
+  res.send('This is home');  
+});
 
 //listiner
 app.listen(3001,()=>{
     console.log('Server is running on port 3001');
 })
+
+//https://www.youtube.com/watch?v=gPp65W7tnFM&list=PLHiZ4m8vCp9PHnOIT7gd30PCBoYCpGoQM&index=18&ab_channel=LearnwithSumit-LWS-Bangladesh
